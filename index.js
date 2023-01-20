d31401d15a411ad3b0065673b582b2a18e75eed4
Christian Iredia <101851819+christianiredia@users.noreply.github.com>
2023-01-20 00:53:23 -0600

    Secret number is displayed only when the guess is correct

--- index.js
+++ index.js
@@ -9,7 +9,6 @@
 
 const secretNumber = Math.floor(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector(".number").textContent = secretNumber;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -21,6 +20,7 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(".message").textContent = "CORRECT NUMBER!!";
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
+    document.querySelector(".number").textContent = secretNumber;
     //When input is greater than the number:
   } else if (guess > secretNumber) {
     if (score > 1) {
